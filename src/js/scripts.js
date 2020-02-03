a96002a32a0e717cb96736f96406d9bcb14dcb71
owleek <owleek@mail.ru>
2020-02-04 02:26:50 +0300

    fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
+'use strict';
+
 $(document).ready(function(){
+    $('#ef1 .btn').on('click', function(e){
+        e.preventDefault();
+        $(this).closest('.form__content').fadeOut(300);
+        $('#ef1 .form__success').slideDown(1000);
+    })
 });
